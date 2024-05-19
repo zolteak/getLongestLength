e5f977fae405f6ba86c8df0acca799310ec950a1
zolteak <144640967+zolteak@users.noreply.github.com>
2024-05-19 11:17:58 +0800

    Update getLongestLength.js

--- getLongestLength.js
+++ getLongestLength.js
@@ -1,16 +1,18 @@
-const getLongestLength = (string) => {
+const getLongestLength = (text) => {
     let len = 0;
     let max = 0;
     let flag = 0;
     let mass = [];
-    for (let i = 0; i < string.length+1; i++){
+    let result = 0;
+
+    for (let i = 0; i < text.length+1; i++){
 
         if (len > max){
             max = len;
         }
         if(flag!==0){
             for (let j = 0; j <= i; j++){
-                if (string[i] == mass[j]){
+                if (text[i] == mass[j]){
                     len = 0;
                     mass.splice(0,mass.length);
                     flag=0;
@@ -19,8 +21,10 @@ const getLongestLength = (string) => {
         }   
         flag++;
         len++;
-        mass.push(string[i]);
+        mass.push(text[i]);
     }
+
+    result = max;
 
 
     let len2 = 0;
@@ -28,13 +32,13 @@ const getLongestLength = (string) => {
     let flag2 = 0;
     let mass2 = [];
 
-    for (let i = string.length-1; i >= 0; i--){
+    for (let i = text.length-1; i >= 0; i--){
         if (len2 > max2){
             max2 = len2;
         }       
         if(flag2!==0){
             for (let j = 0; j <= i; j++){
-                if (string[i] === mass2[j]){
+                if (text[i] === mass2[j]){
                     len2 = 0;
                     mass2.splice(0,mass2.length);
                     flag2=0;
@@ -43,15 +47,14 @@ const getLongestLength = (string) => {
         }
         flag2++;
         len2++;
-        mass2.push(string[i]);
+        mass2.push(text[i]);
     }
 
-    if (max > max2) {
-        return max;
+    if (max < max2) {
+        result = max2;
     }
-    else {
-        return max2;
-    }
-};
+
+    return result;
+}  
 
 exports.getLongestLength = getLongestLength;
